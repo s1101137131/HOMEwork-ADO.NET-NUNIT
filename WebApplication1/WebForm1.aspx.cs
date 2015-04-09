b22a0b9105cbeda30f248194506f840a149f4be1
Yuan <pledgeding19931011@gmail.com>
2015-04-10 01:06:42 +0800

    new

---
+++ WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    /// <summary>
+    /// DataReader
+    /// </summary>
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            btnSearch_Click(null, null);
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            lbResult.Items.Clear();
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["DataBaseConnectionString"].ConnectionString))
+            {
+                cn.Open();
+                using (SqlCommand cmd = cn.CreateCommand())
+                {
+                    cmd.CommandText = "select * from Student where Stu_Num like @Stu_Num";
+                    cmd.Parameters.Add(new SqlParameter("@Stu_Num", "%" + txtSearch.Text + "%"));
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while ((dr.Read()))
+                        {
+                            lbResult.Items.Add(dr.GetSqlString(0).ToString() + "\t" + dr.GetSqlString(1).ToString());
+                        }
+                        dr.Close();
+                    }
+                }
+
+            }
+        }
+
+    }
+}
---
+++ WebApplication1/WebForm3.aspx.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    /// <summary>
+    /// DataSet
+    /// </summary>
+    public partial class WebForm3 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            btnSearch_Click(null, null);
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["DataBaseConnectionString"].ConnectionString))
+            {
+                using (SqlCommand cmd = cn.CreateCommand())
+                {
+                    cmd.CommandText = "select * from Student where Stu_Num like @Stu_Num";
+                    cmd.Parameters.Add(new SqlParameter("@Stu_Num", "%" + txtSearch.Text + "%"));
+
+                    using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                    {
+                        DataSet ds = new DataSet();
+                        da.Fill(ds);
+                        gvResult.DataSource = ds;
+                        gvResult.DataMember = "Table";
+                        gvResult.DataBind();
+                        da.Fill(ds);
+                    }
+                }
+            }
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["DataBaseConnectionString"].ConnectionString))
+            {
+                using (SqlCommand cmd = cn.CreateCommand())
+                {
+                    cmd.CommandText = "select * from Student";
+                    cmd.Parameters.Add(new SqlParameter("@Stu_Num", "%" + txtSearch.Text + "%"));
+                    cmd.Parameters.Add(new SqlParameter("@Stu_Name", "%" + txtSearch.Text + "%"));
+                    cmd.Parameters.Add(new SqlParameter("@Stu_Phone", "%" + txtSearch.Text + "%"));
+
+                    using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                    {
+                        //自動產生Insert, Update, DeleteCommand
+                        SqlCommandBuilder builder = new SqlCommandBuilder(da);
+                        //builder.GetInsertCommand()
+                        //builder.GetUpdateCommand()
+                        //builder.GetDeleteCommand()
+
+                        DataSet ds = new DataSet();
+                        ds.Clear();
+                        da.Fill(ds);
+                        DataTable dt = ds.Tables["Table"];
+                        DataRow dr = dt.NewRow();
+                        dr["Stu_Num"] = txtNum.Text;
+                        dr["Stu_Name"] = txtName.Text;
+                        dr["Stu_Phone"] = txtPhone.Text;
+
+                        dt.Rows.Add(dr);
+                        da.Update(dt);
+
+                        btnSearch_Click(null, null);
+                    }
+                }
+            }
+        }
+
+        protected void btnEdit_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["DataBaseConnectionString"].ConnectionString))
+            {
+                using (SqlCommand cmd = cn.CreateCommand())
+                {
+                    cmd.CommandText = "select * from Student";
+
+                    using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                    {
+                        //自動產生Insert, Update, DeleteCommand
+                        SqlCommandBuilder builder = new SqlCommandBuilder(da);
+                        //builder.GetInsertCommand()
+                        //builder.GetUpdateCommand()
+                        //builder.GetDeleteCommand()
+
+                        DataSet ds = new DataSet();
+                        ds.Clear();
+                        da.Fill(ds);
+                        DataTable dt = ds.Tables[0];
+                        DataRow dr = dt.Select(string.Format("Stu_Num = {0}", txtE_Num.Text)).First();
+                        dr["Stu_Num"] = txtE_Num.Text;
+                        dr["Stu_Name"] = txtE_Name.Text;
+                        dr["Stu_Phone"] = txtE_Phone.Text;
+                        da.Update(dt);
+
+                        btnSearch_Click(null, null);
+                    }
+                }
+            }
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["DataBaseConnectionString"].ConnectionString))
+            {
+                using (SqlCommand cmd = cn.CreateCommand())
+                {
+                    cmd.CommandText = "select * from Student";
+
+                    using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                    {
+                        //自動產生Insert, Update, DeleteCommand
+                        SqlCommandBuilder builder = new SqlCommandBuilder(da);
+                        //builder.GetInsertCommand()
+                        //builder.GetUpdateCommand()
+                        //builder.GetDeleteCommand()
+
+                        DataSet ds = new DataSet();
+                        ds.Clear();
+                        da.Fill(ds);
+                        DataTable dt = ds.Tables[0];
+                        DataRow dr = dt.Select(string.Format("Stu_Num = {0}", txtD_Num.Text)).First();
+                        dr.Delete();
+                        da.Update(dt);
+
+                        btnSearch_Click(null, null);
+                    }
+                }
+            }
+        }
+    }
+}
